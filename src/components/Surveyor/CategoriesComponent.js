474c50725f41dab5861baa79c4ac1a6bd0bbb7d1
nught <paternenught@gmail.com>
2021-07-27 11:15:26 +0200

    add delete button

--- src/components/Surveyor/CategoriesComponent.js
+++ src/components/Surveyor/CategoriesComponent.js
@@ -5,7 +5,52 @@ export default function CategoriesComponent(props) {
     
     const [isModalOpen, setIsModalOpen] = useState(false);
     const togglerModal = () => setIsModalOpen(!isModalOpen);
+    //let addClass = (event) =>{
+        // let id = event.target.id;
+        // //let element = document.querySelector("." + id);
+        // let element = document.getElementsByClassName(id)[0];
+        // element.classList.remove("hideIt");
 
+    //}
+    //let removeClass = (event) =>{
+        // let id = event.target.id;
+        // //let element = document.querySelector("." + id);
+        // let element = document.getElementsByClassName(id)[0];
+        // element.classList.add("hideIt");
+    //}
+
+    // let element = null;
+    //
+    // if(document.readyState === "complete") {
+    //     // Fully loaded!
+    //     element = document.querySelector(".categoryButton");
+    //
+    //     element.addEventListener("mouseover", event => {
+    //         console.log("Mouse in");
+    //         element.classList.add("showDeleteAndEdit");
+    //     });
+    //
+    //     element.addEventListener("mouseout", event => {
+    //         console.log("Mouse out");
+    //         element.classList.remove("showDeleteAndEdit");
+    //     });
+    // }
+    // else if(document.readyState === "interactive") {
+    //     // DOM ready! Images, frames, and other subresources are still downloading.
+    // }
+    // else {
+    //     // Loading still in progress.
+    //     // To wait for it to complete, add "DOMContentLoaded" or "load" listeners.
+    //
+    //     window.addEventListener("DOMContentLoaded", () => {
+    //         // DOM ready! Images, frames, and other subresources are still downloading.
+    //     });
+    //
+    //     window.addEventListener("load", () => {
+    //         // Fully loaded!
+    //     });
+    // }
+    //
 
     let handleClick=(event) =>{
         let category = event.target.id;
@@ -18,13 +63,36 @@ export default function CategoriesComponent(props) {
         togglerModal();
     }
     let categories = props.categories.map((category, index) =>{
+        let btn = "";
+        let [cCategory,action] =  props.showDelete.split(',');
+        if(category===cCategory && action ==='showIt' && category !=='Default'){
+            btn = category===props.selectedCategory?<Button key={category + index} id={category} onClick={handleClick} className="w-100 bg-light text-dark d-flex justify-content-end align-items-center">
+                    {category}
+                    <div className={"btn-group ml-5 pl-5"}>
+                        <Button className="bg-light text-dark"><span className="fa fa-edit"></span></Button>
+                        <Button className="bg-danger"><span className="fa fa-trash"></span></Button>
+                    </div>
+                   </Button>
+                :<Button key={category + index} id={category} onClick={handleClick} className="w-100 d-flex justify-content-end align-items-center">{category}
+                    <div className={"btn-group  ml-5 pl-5"}>
+                        <Button className="bg-light text-dark"><span className="fa fa-edit"></span></Button>
+                        <Button className="bg-danger"><span className="fa fa-trash"></span></Button>
+                    </div>
+                </Button>
+        }
+        else{
+            btn = category===props.selectedCategory?<Button key={category + index} id={category} onClick={handleClick} className="w-100 bg-light text-dark categoryButton">
+                    {category}
+                    <i class="fa fa-angle-double-right ml-3 "></i></Button>
+                :<Button key={category + index} id={category} onClick={handleClick} className="w-100 categoryButton">{category} </Button>
+        }
 
         return(
-            <div className="m-1">
-                {category===props.selectedCategory?<Button key={category + index} id={category} onClick={handleClick} className="w-100 bg-light text-dark">{category} <i class="fa fa-angle-double-right ml-3"></i></Button>:<Button key={category + index} id={category} onClick={handleClick} className="w-100">{category}</Button>}
-                
+            <div className="m-1" id={category + ",categoryB"} onMouseEnter={props.handleEnterAndLeave} onMouseLeave={props.handleEnterAndLeave}>
+                {btn}
+
             </div>
-            
+
         )
     })
     return (
@@ -49,6 +117,17 @@ export default function CategoriesComponent(props) {
                 <div>
                     <Button onClick={togglerModal} className="m-1"><span className="fa fa-plus" ></span> Create new category</Button>
                 </div>
+                {/*<div>*/}
+                {/*    <div className="m-1">*/}
+                {/*        {"Default" === props.selectedCategory ?*/}
+                {/*            <Button key={"Default + index"} id={"Default"} onClick={handleClick}*/}
+                {/*                    className="w-100 color1 text-dark" data-toggle="tooltip" data-placement="top" title="Selected by default">{"Default"} <i*/}
+                {/*                className="fa fa-angle-double-right ml-3"></i></Button> :*/}
+                {/*            <Button key={"Default + index"} id={"Default"} onClick={handleClick}*/}
+                {/*                    className="w-100 color1 text-dark" data-toggle="tooltip" data-placement="top" title="Selected by default">{"Default"}</Button>}*/}
+
+                {/*    </div>*/}
+                {/*</div>*/}
                 <div>
                     {categories}
                 </div>
